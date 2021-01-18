0d31b44c2287dadfc435aee65876a8089f73a885
yourhuckleberry <githuckleberry@gmail.com>
2021-01-17 17:54:33 -0800

    Users Model created
    User  route created

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
+
+const User = require('../models/User');
+
 // @route POST api/users
 // @desc  Register a user
 // @access Public
