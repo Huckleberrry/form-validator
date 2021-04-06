1a87710ef8d3893cd92a6da7f2ea62c63a061258
yourhuckleberry <githuckleberry@gmail.com>
2021-04-06 14:41:11 -0700

    Alert Context, State & Component

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,23 @@ import Navbar from "./components/layout/Navbar"
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
+import AlertState from './context/alert/AlertState';
 import AuthState from './context/auth/authState';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 import './App.css';
 
 const  App = () => {
   return (
     <AuthState>
     <ContactState>
+      <AlertState>
     <Router>
       <Fragment>
         <Navbar />
         <div className="container">
+          <Alerts />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path="/about" component={About} />
@@ -29,6 +33,7 @@ const  App = () => {
         </div>
       </Fragment>
     </Router>
+    </AlertState>
     </ContactState>
     </AuthState>
   )
